2f6b887aa77cb9ff341a7266213d6b3b47b160e8
zxj_pc <zhuxiujia@qq.com>
2021-04-11 17:47:01 +0800

    fix ops i32 to i64

--- src/ops.rs
+++ src/ops.rs
@@ -119,6 +119,12 @@ impl PartialEq<str> for Value {
     }
 }
 
+impl PartialEq<str> for &Value {
+    fn eq(&self, other: &str) -> bool {
+        self.as_str().unwrap_or("").eq(other)
+    }
+}
+
 impl PartialEq<Value> for String {
     fn eq(&self, other: &Value) -> bool {
         other.as_str().unwrap_or("").eq(self)
@@ -131,6 +137,13 @@ impl PartialEq<String> for Value {
     }
 }
 
+impl PartialEq<String> for &Value {
+    fn eq(&self, other: &String) -> bool {
+        other.eq(self.as_str().unwrap_or(""))
+    }
+}
+
+
 
 impl PartialEq<Value> for i32 {
     fn eq(&self, other: &Value) -> bool {
@@ -139,6 +152,12 @@ impl PartialEq<Value> for i32 {
 }
 
 impl PartialEq<i32> for Value {
+    fn eq(&self, other: &i32) -> bool {
+        (*other as i64).eq(&self.as_i64().unwrap_or(0))
+    }
+}
+
+impl PartialEq<i32> for &Value {
     fn eq(&self, other: &i32) -> bool {
         (*other as i64).eq(&self.as_i64().unwrap_or(0))
     }
@@ -156,6 +175,12 @@ impl PartialEq<i64> for Value {
     }
 }
 
+impl PartialEq<i64> for &Value {
+    fn eq(&self, other: &i64) -> bool {
+        (*other as i64).eq(&self.as_i64().unwrap_or(0))
+    }
+}
+
 impl PartialEq<Value> for f32 {
     fn eq(&self, other: &Value) -> bool {
         (*self as i64).eq(&other.as_i64().unwrap_or(0))
@@ -167,6 +192,13 @@ impl PartialEq<f32> for Value {
         (*other as i64).eq(&self.as_i64().unwrap_or(0))
     }
 }
+
+impl PartialEq<f32> for &Value {
+    fn eq(&self, other: &f32) -> bool {
+        (*other as i64).eq(&self.as_i64().unwrap_or(0))
+    }
+}
+
 
 impl PartialEq<Value> for f64 {
     fn eq(&self, other: &Value) -> bool {
@@ -180,6 +212,12 @@ impl PartialEq<f64> for Value {
     }
 }
 
+impl PartialEq<f64> for &Value {
+    fn eq(&self, other: &f64) -> bool {
+        (*other as i64).eq(&self.as_i64().unwrap_or(0))
+    }
+}
+
 
 impl PartialEq<Value> for serde_json::Value {
     fn eq(&self, other: &Value) -> bool {
@@ -188,6 +226,12 @@ impl PartialEq<Value> for serde_json::Value {
 }
 
 impl PartialEq<serde_json::Value> for Value {
+    fn eq(&self, other: &serde_json::Value) -> bool {
+        (*other).eq(&self.inner)
+    }
+}
+
+impl PartialEq<serde_json::Value> for &Value {
     fn eq(&self, other: &serde_json::Value) -> bool {
         (*other).eq(&self.inner)
     }
