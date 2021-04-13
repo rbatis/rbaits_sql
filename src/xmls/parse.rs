2fa57db2002b86e061dff5739034c2dbb309d234
zxj_pc <zhuxiujia@qq.com>
2021-04-13 23:13:28 +0800

    add test

--- src/xmls/parse.rs
+++ src/xmls/parse.rs
@@ -1,47 +1,55 @@
-
-
-
-
 #[cfg(test)]
-mod test{
+mod test {
     use std::fs::File;
     use std::io::BufReader;
     use xml::EventReader;
     use xml::reader::{XmlEvent, Error};
 
     #[test]
-    fn test_load_xml(){
+    fn test_load_xml() {
         let file = File::open("example/example.xml").unwrap();
         let file = BufReader::new(file);
         let parser = EventReader::new(file);
         let mut depth = 0;
         for e in parser {
             match e {
-                Ok(XmlEvent::StartElement { name, .. }) => {
-                    println!("{}+{}", depth, name);
+                Ok(XmlEvent::StartElement { name, attributes, namespace }) => {
+                    let mut attrs = String::new();
+                    for attr in attributes {
+                        attrs.push_str(&format!(" {} = \"{}\" ", attr.name, attr.value));
+                    }
+                    println!("{}<{} {} >", depth_to_space(depth), name, attrs);
                     depth += 1;
                 }
                 Ok(XmlEvent::EndElement { name }) => {
                     depth -= 1;
-                    println!("{}-{}", depth, name);
+                    println!("{}</{}>", depth_to_space(depth), name);
                 }
                 Err(e) => {
                     println!("Error: {}", e);
                     break;
                 }
                 Ok(XmlEvent::Characters(s)) => {
-                    println!("Characters:{}",s);
+                    println!("{}", s);
                 }
                 Ok(XmlEvent::Comment(s)) => {
-                    println!("Comment:{}",s);
+                    println!("{}", s);
                 }
                 Ok(XmlEvent::CData(s)) => {
-                    println!("CData:{}",s);
+                    println!("{}", s);
                 }
                 _ => {
-                   // println!("DefaultStr");
+                    // println!("DefaultStr");
                 }
             }
         }
+    }
+
+    fn depth_to_space(depth: i32) -> String {
+        let mut s = String::new();
+        for _ in 0..depth {
+            s.push_str(" ");
+        }
+        return s;
     }
 }
