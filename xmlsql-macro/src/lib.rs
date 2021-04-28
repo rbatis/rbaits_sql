2bc019c8cb5f0fb44d25d4a499f0ff07995ca536
zxj_pc <zhuxiujia@qq.com>
2021-04-29 00:52:57 +0800

    fix trim node

--- xmlsql-macro/src/lib.rs
+++ xmlsql-macro/src/lib.rs
@@ -5,7 +5,7 @@ use syn::{ItemFn, DataStruct};
 use crate::proc_macro::TokenStream;
 
 mod func;
-mod parse;
+mod parser;
 mod xml_loader;
 
 #[proc_macro_attribute]
@@ -26,7 +26,7 @@ pub fn expr(args: TokenStream, func: TokenStream) -> TokenStream {
 pub fn xml(args: TokenStream, func: TokenStream) -> TokenStream {
     //let args = parse_macro_input!(args as AttributeArgs);
     let target_fn = syn::parse(func).unwrap();
-    let stream = parse::impl_fn(&target_fn, args);
+    let stream = parser::impl_fn(&target_fn, args);
     #[cfg(feature = "debug_mode")]
         {
             println!("............gen macro xml:\n {}", stream);
