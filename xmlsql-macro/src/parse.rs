9dc458d318594a28d644e9457961d15d92c1e4b9
zxj_pc <zhuxiujia@qq.com>
2021-04-28 21:51:22 +0800

    format parse

--- xmlsql-macro/src/parse.rs
+++ xmlsql-macro/src/parse.rs
@@ -11,7 +11,7 @@ use std::io::Read;
 use std::collections::HashMap;
 use crate::xml_loader::{load_xml, Element};
 
-const  example_data:&'static str=include_str!("../../example/example.xml");
+const example_data: &'static str = include_str!("../../example/example.xml");
 
 fn parse_str(arg: &str) -> TokenStream {
     let datas = load_xml(arg);
@@ -28,44 +28,46 @@ fn parse_str(arg: &str) -> TokenStream {
 fn parse(arg: &Vec<Element>, methods: &mut proc_macro2::TokenStream) -> proc_macro2::TokenStream {
     let mut body = quote! {};
     for x in arg {
-        if x.tag.eq("mapper") {
-            return parse(&x.childs, methods);
-        }
-        if x.tag.eq("") {
-            let s = x.data.to_owned();
-            body = quote!(
+        match x.tag.as_str() {
+            "mapper" => {
+                return parse(&x.childs, methods);
+            }
+            "" => {
+                let mut s = x.data.to_owned();
+                s = format!(" {} ", s.trim());
+                body = quote!(
                         #body
                          sql=sql+#s;
                        );
-        }
-        if x.tag.eq("if") {
-            let test_value = x.attributes.get("test").expect("if element must be have test field!");
-            let method_name_string = encode(&test_value).replace("_", "__").replace("=", "_");
-            let method_name = Ident::new(&method_name_string, Span::call_site());
-            let test_value = test_value.replace(" and ", " && ");
-            let test_value = test_value.replace(" or ", " && ");
-            let method_impl = crate::func::impl_fn(&method_name.to_string(), &format!("\"{}\"", test_value));
-            if !methods.to_token_stream().to_string().contains(&method_name_string) {
-                *methods = quote! {
-                                             #methods
-                                             #method_impl
-                                          };
             }
-            if x.childs.len() != 0 {
-                let if_tag_body = parse(&x.childs, methods);
-                body = quote! {
+            "if" => {
+                let test_value = x.attributes.get("test").expect("if element must be have test field!");
+                let method_name_string = encode(&test_value).replace("_", "__").replace("=", "_");
+                let method_name = Ident::new(&method_name_string, Span::call_site());
+                let test_value = test_value.replace(" and ", " && ");
+                let test_value = test_value.replace(" or ", " && ");
+                let method_impl = crate::func::impl_fn(&method_name.to_string(), &format!("\"{}\"", test_value));
+                if !methods.to_token_stream().to_string().contains(&method_name_string) {
+                    *methods = quote! {
+                                   #methods
+                                   #method_impl
+                             };
+                }
+                if x.childs.len() != 0 {
+                    let if_tag_body = parse(&x.childs, methods);
+                    body = quote! {
                               #body
                               if #method_name(arg).as_bool().unwrap_or(false) {
                                    #if_tag_body
                                 }
-                              };
+                          };
+                }
             }
-        }
-        if x.tag.eq("select") {
-            let id = x.attributes.get("id").expect("select element must be have id!");
-            let method_name = Ident::new(id, Span::call_site());
-            let child_body = parse(&x.childs, methods);
-            let mut select = quote! {
+            "select" => {
+                let id = x.attributes.get("id").expect("select element must be have id!");
+                let method_name = Ident::new(id, Span::call_site());
+                let child_body = parse(&x.childs, methods);
+                let mut select = quote! {
                             pub fn #method_name (arg:&serde_json::Value) -> (String,Vec<serde_json::Value>) {
                                let mut sql = String::with_capacity(1000);
                                let mut args = vec![];
@@ -73,24 +75,18 @@ fn parse(arg: &Vec<Element>, methods: &mut proc_macro2::TokenStream) -> proc_mac
                                return (sql,args);
                             }
                         };
-            body = quote! {
+                body = quote! {
                             #body
                             #select
                         };
+            }
+            _ => {}
         }
     }
 
     println!("gen methods:\n{}", methods);
     println!("gen fn:----start----\n{}\n----end----\n", body);
     return body.into();
-}
-
-fn depth_to_space(depth: i32) -> String {
-    let mut s = String::new();
-    for _ in 0..depth {
-        s.push_str(" ");
-    }
-    return s;
 }
 
 pub(crate) fn impl_fn(f: &ItemFn, args: crate::proc_macro::TokenStream) -> TokenStream {
