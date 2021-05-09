d011e77bce7a77009fbdbfc498718c91ffcf4a37
zxj_pc <zhuxiujia@qq.com>
2021-05-09 22:19:59 +0800

    add executor.rs

--- src/executor.rs
+++ src/executor.rs
@@ -4,23 +4,23 @@ use serde::{Serialize, Deserialize};
 use async_trait::async_trait;
 
 #[derive(Serialize, Deserialize, Clone, Debug)]
-pub struct ExecutorExecResult {
+pub struct BackendExecResult {
     pub rows_affected: u64,
     pub last_insert_id: Option<i64>,
 }
 
 #[async_trait]
-pub trait Executor {
+pub trait Backend {
     /// fetch result(row sql)
     async fn fetch<T>(&self, context_id: &str, sql: &str) -> Result<T, Error>
         where
             T: DeserializeOwned;
 
     /// exec sql(row sql)
-    async fn exec(&self, context_id: &str, sql: &str) -> Result<ExecutorExecResult, Error>;
+    async fn exec(&self, context_id: &str, sql: &str) -> Result<BackendExecResult, Error>;
 
     /// exec sql(prepare sql)
-    async fn exec_prepare(&self, context_id: &str, sql: &str, args: &Vec<serde_json::Value>) -> Result<ExecutorExecResult, Error>;
+    async fn exec_prepare(&self, context_id: &str, sql: &str, args: &Vec<serde_json::Value>) -> Result<BackendExecResult, Error>;
 
     /// fetch result(prepare sql)
     async fn fetch_prepare<T>(&self, context_id: &str, sql: &str, args: &Vec<serde_json::Value>) -> Result<T, Error> where T: DeserializeOwned;
