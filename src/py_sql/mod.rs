d4b4dd81504cd7c65ce71c92f62fde2a7b355ccc
zxj_pc <zhuxiujia@qq.com>
2021-06-06 01:55:23 +0800

    add py_sql impl

--- src/py_sql/mod.rs
+++ src/py_sql/mod.rs
@@ -1,16 +1,54 @@
+pub mod string_node;
+pub mod if_node;
+pub mod trim_node;
+pub mod foreach_node;
+pub mod choose_node;
+pub mod otherwise_node;
+pub mod when_node;
+pub mod bind_node;
+pub mod set_node;
+pub mod where_node;
+pub mod print_node;
+pub mod impl_node;
+
+use crate::py_sql::string_node::StringNode;
+use crate::py_sql::if_node::IfNode;
+use crate::py_sql::trim_node::TrimNode;
+use crate::py_sql::foreach_node::ForEachNode;
+use crate::py_sql::choose_node::ChooseNode;
+use crate::py_sql::otherwise_node::OtherwiseNode;
+use crate::py_sql::when_node::WhenNode;
+use crate::py_sql::bind_node::BindNode;
+use crate::py_sql::set_node::SetNode;
+use crate::py_sql::where_node::WhereNode;
+use crate::py_sql::print_node::PrintNode;
+
+
 #[derive(Clone, Debug)]
 pub enum NodeType {
     Null,
-    // NString(StringNode),
-    // NIf(IfNode),
-    // NTrim(TrimNode),
-    // NForEach(ForEachNode),
-    // NChoose(ChooseNode),
-    // NOtherwise(OtherwiseNode),
-    // NWhen(WhenNode),
-    // NBind(BindNode),
-    // NSet(SetNode),
-    // NWhere(WhereNode),
-    // NPrint(PrintNode),
+    NString(StringNode),
+    NIf(IfNode),
+    NTrim(TrimNode),
+    NForEach(ForEachNode),
+    NChoose(ChooseNode),
+    NOtherwise(OtherwiseNode),
+    NWhen(WhenNode),
+    NBind(BindNode),
+    NSet(SetNode),
+    NWhere(WhereNode),
+    NPrint(PrintNode),
 }
 
+
+pub trait Name {
+    fn name() -> &'static str;
+}
+
+pub trait DefName {
+    fn def_name() -> &'static str;
+}
+
+pub trait ParsePySql {
+    fn parse(arg: &str) -> Result<Vec<NodeType>, crate::error::Error>;
+}
