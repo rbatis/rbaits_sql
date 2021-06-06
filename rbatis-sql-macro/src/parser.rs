d48d113ac5a56bfdcf166a33b9f3c4de876f3d8d
zxj_pc <zhuxiujia@qq.com>
2021-06-06 22:57:28 +0800

    add todo

--- rbatis-sql-macro/src/parser.rs
+++ rbatis-sql-macro/src/parser.rs
@@ -56,7 +56,8 @@ fn parse(arg: &Vec<Element>, methods: &mut proc_macro2::TokenStream, block_name:
        for x in &s {
            data.push_str(x);
            if s.len()!=(index+1){
-                data.push_str(&format!(" {} ",index));
+                data.push_str(&format!(" ${} ",index));
+                data.push
            }
            index+=1;
        }
